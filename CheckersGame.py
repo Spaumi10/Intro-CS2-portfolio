994c1c092249925cf05c8b5989aa6e39fc6905e4
Michael Spaulding <mikespaulding3@gmail.com>
2023-02-24 19:15:09 -0800

    Created board with start pieces

--- CheckersGame.py
+++ CheckersGame.py
@@ -1,11 +1,57 @@
+# Author: Michael Spaulding
+# GitHub username: Spaumi10
+# Date: 02/24/2023
+# Description:
+
+
 class Checkers:
     """
     Represents a checkers game, including board and gameplay.
     """
 
+    BLACK_CHECKERS = 12
+    WHITE_CHECKERS = 12
+
     def __init__(self):
-        self._BLACK_CHECKERS = 12
-        self._WHITE_CHECKERS = 12
+        self._board = []
+        # TODO Shorten code for creating a board w/ pieces.
+        # Creates board and places starting pieces.
+        for i in range(8):
+            temp_list = []
+            for i in range(8):
+                # White piece placement
+                # Places white pieces on 1st and 3rd row from top.
+                if len(self._board) % 2 == 0 and len(self._board) < 3:
+                    if len(temp_list) % 2 == 1:
+                        temp_list.append("White")
+                    else:
+                        temp_list.append(None)
+                # Places white pieces on 2nd row from top.
+                elif len(self._board) % 2 == 1 and len(self._board) < 3:
+                    if len(temp_list) % 2 == 0:
+                        temp_list.append("White")
+                    else:
+                        temp_list.append(None)
+
+                # Black piece placement
+                # Places black pieces on 6th and 8th row from top.
+                elif len(self._board) % 2 == 0 and len(self._board) > 4:
+                    if len(temp_list) % 2 == 1:
+                        temp_list.append("Black")
+                    else:
+                        temp_list.append(None)
+                # Places black pieces on 7th row from top.
+                elif len(self._board) % 2 == 1 and len(self._board) > 4:
+                    if len(temp_list) % 2 == 0:
+                        temp_list.append("Black")
+                    else:
+                        temp_list.append(None)
+
+                # Fills in empty spaces
+                else:
+                    temp_list.append(None)
+
+            self._board.append(temp_list)
 
     def create_player(self, player_name, piece_color):
         """Returns a player object with name and piece color."""
@@ -53,3 +99,20 @@ class Player:
 
     def get_captured_pieces_count(self):
         """Returns the number of opponent peces that the player has captured."""
+
+
+game = Checkers()
+for row in game._board:
+    print(row)
+
+# Player1 = game.create_player("Adam", "White")
+
+# Player2 = game.create_player("Lucy", "Black")
+
+# game.play_game("Lucy", (5, 6), (4, 7))
+
+# game.play_game("Adam", (2,1), (3,0))
+
+# game.get_checker_details((3,1))
+
+# Player1.get_captured_pieces_count()
